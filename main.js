443feeed737b517444dee2a6442c19d8fc3a307e
Owen Yang <owentheyanger@gmail.com>
2025-02-18 20:21:30 -0800

    final bug fixes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var margin = { top: 60, right: 30, bottom: 60, left: 150 },
+var margin = { top: 120, right: 30, bottom: 60, left: 150 },
     width  = 1200 - margin.left - margin.right,
     height = 900  - margin.top  - margin.bottom;
 
